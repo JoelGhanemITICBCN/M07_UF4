822c40bcf7e2bd9b64d40822730f6c3f2fb84bab
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-03-06 19:12:21 +0100

    Funciona el redireccionamiento de views

---
+++ TIC_BCN_JJGG/centre/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('',views.index, name='index'),
+        path('students',views.students, name='students'),
+        path('teachers',views.teachers, name='teachers')
+]
--- TIC_BCN_JJGG/centre/views.py
+++ TIC_BCN_JJGG/centre/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def index(request):
+    return HttpResponse("Hola Index keloke")
+
 def students(request):
     return HttpResponse("Hola student keloke")
 
