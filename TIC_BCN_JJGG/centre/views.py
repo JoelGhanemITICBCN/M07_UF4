232c03845c86748b1341e04b31ed0cdaffaf75cc
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-03-08 16:38:20 +0100

    Funciona el render

--- TIC_BCN_JJGG/centre/views.py
+++ TIC_BCN_JJGG/centre/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.template import Context, loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse("Hola Index keloke")
+    professor = {"nom":"Roger","surname":"Sobrino", "age": "37"}
+    return render(request, 'index.html', {'nom':professor["nom"], 'surname':professor["surname"], 'age':professor["age"]})
 
 def students(request):
     return HttpResponse("Hola student keloke")
