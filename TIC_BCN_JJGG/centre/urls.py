b2532ed5edd72541841a20978f9ba30a27b7aded
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-03-13 17:28:11 +0100

    Inserits 4 rprofes

--- TIC_BCN_JJGG/centre/urls.py
+++ TIC_BCN_JJGG/centre/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
         path('',views.index, name='index'),
-        path('proff',views.students, name='proff'),
-        path('users',views.teachers, name='users')
+        path('teachers',views.teachers, name='teachers'),
+        path('students',views.users, name='users')
 ]
--- TIC_BCN_JJGG/centre/views.py
+++ TIC_BCN_JJGG/centre/views.py
@@ -5,16 +5,54 @@ from django.template import Context, loader
 # Create your views here.
 
 def index(request):
-    professor = {"nom":"Roger","surname":"Sobrino", "age": "37"}
+    professor = {}
     return render(request, 'index.html', {'nom':professor["nom"], 'surname':professor["surname"], 'age':professor["age"]})
 
 def students(request):
-    students = []
+    students = [{"nom"}]
     context = {'s':students}
     return render(request, 'users.html', context)
 
 def teachers(request):
-    teachers = []
+    teachers = [{
+  {
+            'id': 1,
+            'nom': 'Xavi',
+            'cognom1': 'Quesada',
+            'cognom2': 'Puertas',
+            'correu': 'xavi@iticbcn.cat',
+            'curs': 'ASIX2A',
+            'moduls': 'M08, MAH'
+        },
+  {
+            'id': 2,
+            'nom': 'Roger',
+            'cognom1': 'Sobrino',
+            'cognom2': 'Gil',
+            'correu': 'roger@iticbcn.cat',
+            'curs': 'DAM2B',
+            'moduls': 'M07'
+        },
+        {
+            'id': 3,
+            'nom': 'Juanma',
+            'cognom1': 'Sanchez',
+            'cognom2': 'Bel',
+            'correu': 'juanma@iticbcn.cat',
+            'curs': 'DAW2A',
+            'moduls': 'M06'
+        }, 
+        {
+            'id': 4,
+            'nom': 'Oriol',
+            'cognom1': 'Roca',
+            'cognom2': 'Fabra',
+            'correu': 'oriol@iticbcn.cat',
+            'curs': 'DAW2B',
+            'moduls': 'M09'
+        }
+      
+      }]
     context = {'t':teachers}
     return render(request, 'professors.html', context)
 
