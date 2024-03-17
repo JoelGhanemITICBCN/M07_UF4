66bd2849c2d5e35d6f3b371230285f04870f295f
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-03-17 18:38:27 +0100

    He hecho los pasos y las migraciones pero no puedo entrar en el puto pgAdmin

--- TIC_BCN_JJGG/centre/models.py
+++ TIC_BCN_JJGG/centre/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuari(models.Model):
+    nom = models.CharField(max_length=50)
+    cognom = models.CharField(max_length=50)
+    correu = models.CharField(max_length=50)
+    rol = models.CharField(max_length=50)
+    moduils = models.CharField(max_length=50)
