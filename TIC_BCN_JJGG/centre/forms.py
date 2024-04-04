4a6354c17bd0ed97b75f649dc603dee84c116a87
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-04-04 18:49:30 +0200

    Cambiat el nom de la classe

--- TIC_BCN_JJGG/centre/forms.py
+++ TIC_BCN_JJGG/centre/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import Usuari
 
-class PersonForm(ModelForm):
+class UsuariForm(ModelForm):
     class Meta:
         model = Usuari
         fields = '__all__'
